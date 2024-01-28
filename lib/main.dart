987a4bb090f763658d765d40c3b844c069045766
Ricardo <rikialamo@gmail.com>
2024-01-28 12:10:28 +0100

    Funcionalidad añadida

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,8 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,77 +15,110 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.purple,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Scaffold(
-        backgroundColor: Colors.grey[900],
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            const Text(
-              'CONTADOR',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 80.0,
-                fontWeight: FontWeight.bold,
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _resetCounter() {
+    setState(() {
+      _counter = 0;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          const Text(
+            'CONTADOR',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 80.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 16.0),
+          Container(
+            width: 200.0,
+            height: 200.0,
+            child: Center(
+              child: Text(
+                '$_counter',
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 130.0,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-            const SizedBox(height: 16.0),
-            Container(
-              width: 200.0,
-              height: 200.0,
-              child: const Center(
-                child: Text(
-                  '0',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 130.0,
-                    fontWeight: FontWeight.bold,
-                  ),
+          ),
+          const SizedBox(height: 100.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: _incrementCounter,
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.grey[800],
                 ),
+                child: const Text('+'),
               ),
-            ),
-            const SizedBox(height: 186.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.white,
-                    backgroundColor: Colors.grey[800],
-                  ),
-                  child: const Text('+'),
+              ElevatedButton(
+                onPressed: _resetCounter,
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.grey[800],
                 ),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.white,
-                    backgroundColor: Colors.grey[800],
-                  ),
-                  child: const Text('RESET'),
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.white,
-                    backgroundColor: Colors.grey[800],
-                  ),
-                  child: const Text('-'),
-                ),
-              ],
-            ),
-            
-            Text(
-              'By Ricardo Álamo',
-              
-              style: TextStyle(
-                color: Colors.grey[600],
-                fontSize: 10.0,
-                fontWeight: FontWeight.bold,
+                child: const Text('RESET'),
               ),
-            )
-          ],
-        ),
+              ElevatedButton(
+                onPressed: _decrementCounter,
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.grey[800],
+                ),
+                child: const Text('-'),
+              ),
+            ],
+          ),
+          Text(
+            'By Ricardo Álamo',
+            style: TextStyle(
+              color: Colors.grey[600],
+              fontSize: 10.0,
+              fontWeight: FontWeight.bold,
+            ),
+          )
+        ],
       ),
     );
   }
 }
+
